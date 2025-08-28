f4d20e5b5c1d069b466e9d1714a9b000916d2a42
Lewis-mbui <lewmugo14479@gmail.com>
2025-08-28 08:21:54 +0300

    Learn strict and loose equality operators

---
+++ JS/Operators/equality.js
@@ -0,0 +1,8 @@
+// Strict Equality (Type + Value)
+console.log(1 === 1);
+console.log('1' === 1);
+
+// Lose Equality (If types don't match --> converts data type of right operand to that of left operand)
+console.log(1 == 1);
+console.log('1' == 1); 
+console.log(true == 1);
