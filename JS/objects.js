9b0799cca6ce74f7d41268dd7e1ca1165199542b
Lewis-mbui <lewmugo14479@gmail.com>
2025-08-28 07:19:09 +0300

    learn objects

---
+++ JS/objects.js
@@ -0,0 +1,13 @@
+let person = {
+  name: 'Mosh',
+  age: 30
+};
+
+// Dot Notation
+person.name = 'John';
+
+// Bracket Notation
+let selection = 'name';
+person[selection] = 'Mary';
+
+console.log(person.name);
