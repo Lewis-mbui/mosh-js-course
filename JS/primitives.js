185b5b46b22dfb1ef91a194a51f88a8ba248b209
Lewis-mbui <lewmugo14479@gmail.com>
2025-08-28 07:04:46 +0300

    Learn primitive data types

---
+++ JS/primitives.js
@@ -0,0 +1,6 @@
+let name = 'Mosh'; // String Literal
+let age = 30; // Number Literal
+let isApproved = true; // Boolean Literal
+let firstName; // undefined
+let lastName = undefined; // undefined
+let selectedColor = null; // Clear the value of a variable
