02fd7e76dbdb35d979b83e1665e7055c5e8e7b53
Lewis-mbui <lewmugo14479@gmail.com>
2025-08-29 16:29:46 +0300

    Learn string built-in props and methods

---
+++ JS/objects/string-object.js
@@ -0,0 +1,14 @@
+//String Primitive
+const message = 'This is my friend\'s first message';
+
+// String Object
+const another = new String('Hi');
+
+console.log(message.length);
+console.log(message[0]);
+console.log(message.includes('my'));
+console.log(message.startsWith('t'));
+console.log(message.indexOf('my'));
+console.log(message.replace('first', 'second')); //Returns a new string with replaced characters
+console.log(message.toUpperCase());
+console.log(message.split(' '));
