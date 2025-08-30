40fd57a11ae68f314b1fb01f24c9991cbf77021c
Lewis-mbui <lewmugo14479@gmail.com>
2025-08-30 11:03:48 +0300

    Do blog post exercise

---
+++ JS/objects/exercises/blog-post.js
@@ -0,0 +1,13 @@
+let blogPost = {
+  title: 'Dealing with Bugs',
+  body: 'lorem ipsum',
+  author: 'John Smith',
+  views: 20,
+  comments: [
+    {author: 'Jane', comments: 'Lorem'},
+    {author: 'Mary', comments: 'YO'}
+  ],
+  isLive: true
+};
+
+console.log(blogPost);
