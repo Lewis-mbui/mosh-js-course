d73ca029df517c2a3c97ca75313bed6bfe89739b
Lewis-mbui <lewmugo14479@gmail.com>
2025-08-30 12:55:08 +0300

    Learn function default values

---
+++ JS/functions/default-parameters.js
@@ -0,0 +1,18 @@
+// Short circuiting
+/*
+function interest(principal, rate, years) {
+  rate = rate || 3.5;
+  years = years || 5;
+
+  return principal * rate / 100 * years;
+}
+
+console.log(interest(10000));
+*/
+
+// Default values
+function interest(principal, rate = 3.5, years = 5) {
+  return principal * rate / 100 * years;
+}
+
+console.log(interest(10000));
