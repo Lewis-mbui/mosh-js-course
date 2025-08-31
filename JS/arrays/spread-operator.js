610a768df90879b6c275d1a8a9074bd7ee1604a6
Lewis-mbui <lewmugo14479@gmail.com>
2025-08-31 15:44:34 +0300

    Learn the spread operator

---
+++ JS/arrays/spread-operator.js
@@ -0,0 +1,10 @@
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+// const combined = first.concat(second);
+combined = [...first, 'a', ...second, 'b'];
+console.log(combined);
+
+// const copy = combined.splice();
+const copy = [...combined];
+console.log(copy);
