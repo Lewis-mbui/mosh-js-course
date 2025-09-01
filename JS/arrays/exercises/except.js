e78572c33e57f94ec6a7660bea734c19cbbde387
Lewis-mbui <lewmugo14479@gmail.com>
2025-09-01 15:14:53 +0300

    Do except exercise

---
+++ JS/arrays/exercises/except.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3, 4, 1, 1];
+
+const output = except(numbers, [3]);
+console.log(output);
+
+function except(array, excluded) {
+  const output = [];
+
+  for (let value of array) 
+    if (!excluded.includes(value)) 
+      output.push(value);
+  
+  return output;
+}
