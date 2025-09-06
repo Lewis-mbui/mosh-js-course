f3f1dc8ff2ee6c6bb686f7aa2a0da8af50b17899
Lewis-mbui <lewmugo14479@gmail.com>
2025-09-06 07:03:28 +0300

    Learn getters and setters using Object.defineProperty()

---
+++ JS/7-OOP-objects/6-getters-setters.js
@@ -0,0 +1,48 @@
+// Reading private members
+// Method 1:
+/*
+function Circle(radius) {
+  this.radius = radius;
+
+  let defaultLocation = { x: 0, y: 0 };
+
+  this.draw = function() {
+    console.log('draw');
+  };
+  
+  this.getDefaultLocation = function() {
+    return defaultLocation;
+  };
+}
+
+const circle = new Circle(10);
+console.log(circle.getDefaultLocation());
+*/
+
+// Method 2:
+function Circle(radius) {
+  this.radius = radius;
+
+  let defaultLocation = { x: 0, y: 0 };
+
+  this.draw = function() {
+    console.log('draw');
+  };
+  
+  Object.defineProperty(this, 'defaultLocation', {
+    get: function() {
+      return defaultLocation;
+    },
+    set: function(value) {
+      if (!value.x || !value.y)
+        throw new Error('Invalid Location!');
+
+      defaultLocation = value;
+    }
+  });
+}
+
+const circle = new Circle(10);
+console.log(circle.defaultLocation);
+circle.defaultLocation = {x: 12, y:17};
+console.log(circle);
