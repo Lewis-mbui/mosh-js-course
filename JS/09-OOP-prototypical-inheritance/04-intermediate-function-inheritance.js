162ceeb9aae54e211823ec55bcbb393225cc6c63
Lewis-mbui <lewmugo14479@gmail.com>
2025-09-08 15:57:26 +0300

    Learn intermediate function inheritance

---
+++ JS/09-OOP-prototypical-inheritance/04-intermediate-function-inheritance.js
@@ -0,0 +1,36 @@
+function Shape(color) {
+  this.color = color;
+}
+
+Shape.prototype.duplicate = function() {
+  console.log('duplicate');
+}
+
+// Intermediate function inheritance
+function extend(Child, Parent) {
+  Child.prototype = Object.create(Parent.prototype);
+  Child.prototype.constructor = Child;
+}
+
+function Circle(radius, color) {
+  Shape.call(this, color);
+
+  this.radius = radius;
+}
+
+extend(Circle, Shape);
+
+
+Circle.prototype.draw = function() {
+  console.log('draw');
+}
+
+function Square(size) {
+  this.size = size;
+}
+
+extend(Square, Shape);
+
+const sq = new Square();
+
+
